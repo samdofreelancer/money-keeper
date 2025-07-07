831a1faaca1f293a08612376889c34287a30a678
samdortader <111177480+samdortader@users.noreply.github.com>
2025-07-07 21:09:08 +0700

    remove unuse code

--- backend/src/main/java/com/personal/money/management/core/account/infrastructure/persistence/AccountMapper.java
+++ backend/src/main/java/com/personal/money/management/core/account/infrastructure/persistence/AccountMapper.java
@@ -3,12 +3,9 @@ package com.personal.money.management.core.account.infrastructure.persistence;
 import com.personal.money.management.core.account.domain.model.Account;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
-
-    AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
 
     default Account toDomain(AccountEntity entity) {
         if (entity == null) {
