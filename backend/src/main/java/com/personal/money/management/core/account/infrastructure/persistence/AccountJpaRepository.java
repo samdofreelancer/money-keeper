41127627508129ca0308e72d1bb8844e42c17687
samdortader <111177480+samdortader@users.noreply.github.com>
2025-07-08 21:14:40 +0700

    Remove unuse code

--- backend/src/main/java/com/personal/money/management/core/account/infrastructure/persistence/AccountJpaRepository.java
+++ backend/src/main/java/com/personal/money/management/core/account/infrastructure/persistence/AccountJpaRepository.java
@@ -1,12 +1,8 @@
 package com.personal.money.management.core.account.infrastructure.persistence;
 
-import com.personal.money.management.core.account.domain.model.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
