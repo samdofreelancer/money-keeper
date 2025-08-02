b8a2723eda4719539d463ee04a349b896480c941
samdortader <111177480+samdortader@users.noreply.github.com>
2025-08-02 21:29:07 +0700

    correct folder report

--- playwright-code-gen/cucumber.js
+++ playwright-code-gen/cucumber.js
@@ -3,7 +3,7 @@ module.exports = {
     paths: ['src/features/**/*.feature'],
     require: ['src/domains/**/steps/*.ts', 'src/shared/utilities/hooks.ts'],
     requireModule: ['ts-node/register'],
-    format: ['progress-bar', 'html:cucumber-report.html'],
+    format: ['progress-bar', 'html:test-results/cucumber-report.html'],
     parallel: parseInt(process.env.CUCUMBER_PARALLEL_WORKERS || '2')
   }
 };
